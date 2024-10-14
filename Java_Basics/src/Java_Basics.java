a4ba4bcbef47863ca37d62ebd5ec19ac73c54e2e
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-14 17:31:24 -0500

    Initial push

---
+++ Java_Basics/src/Java_Basics.java
@@ -0,0 +1,12 @@
+public class Java_Basics {
+    public static void main(String[] args) throws Exception {
+        // Printing lines in Java
+        System.out.println("Hello, World!");
+        int x = 4;
+
+        System.out.println(x);
+        System.out.println(x + 3);
+        x = 3;
+        System.out.println(x);
+    }
+}
