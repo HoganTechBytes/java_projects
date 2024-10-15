5d86a049ef431080ad0f6a5264f4191f3a8d17c6
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-14 19:57:48 -0500

    Initial push

---
+++ Java_Basics/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String [] args){
+        int number = 1000000000;
+        long bigNumber = 1000000000000000000L;
+        short smallNumber  = 32767;
+        byte verySmallNumber = 127;
+        double decimalVariable = 321.1242;
+        boolean decision = true;
+        char letter = 's';
+        String words = "A string can contain a single letter to paragraphs";
+    }
+
+}
