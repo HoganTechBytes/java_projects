7f72548e89d4b9b1590d9c995a49552b86a8e777
HoganTechBytes <jonhoganbs@gmail.com>
2024-10-16 16:24:26 -0500

    Initial Push

--- Java_Basics/src/FlowControl.java
+++ Java_Basics/src/FlowControl.java
@@ -1,6 +1,50 @@
 public class FlowControl {
     public static void main(String[] args) {
         
-        
+        boolean hungry = true;
+
+        if(hungry){
+            System.out.println("I'm starving!");
+        }else{
+            System.out.println("I'm not hungry.");
+        }
+
+        int x = 0;
+        System.out.println("Variable \"x\" is equal to " + x);
+
+        for(int i = 0; i < 101; ++i){
+            x += i;
+        }
+
+        System.out.println("Now variable \"x\" is equal to " + x);
+
+        int month = 6;
+        String monthString;
+
+        switch(month){
+            case 1:
+                monthString = "January";
+                break;
+            case 2:
+                monthString = "February";
+                break;
+            case 3:
+                monthString = "March";
+                break;
+            case 4:
+                monthString = "April";
+                break;
+            case 5:
+                monthString = "May";
+                break;
+            case 6:
+                monthString = "June";
+                break;
+            default:
+                monthString = "I didn't go past June";
+
+        }
+
+        System.out.println(monthString);
     }
 }
